385c071dda5cfe6b54f19cc788569be326423c6e
victor-de-sousa <victordesousaestudo@gmail.com>
2025-11-14 23:26:12 -0300

    FEAT: Criação de função para filmes populares, ultimos lancamentos. E finalização da função topFive #3

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -5,11 +5,16 @@ import { defineStore } from 'pinia'
 export const useMoviesStore = defineStore('movies', () => {
   const state = reactive({
     movies: [],
+    topFive: [],
+    popularMovies: [],
+    ultimosLancamentos: [],
     page: 1,
-    
-  })
+  });
 
   const movies = computed(() => state.movies)
+  const topFive = computed(() => state.topFive);
+  const popularMovies = computed(() => state.popularMovies);
+  const ultimosLancamentos = computed(() => state.ultimosLancamentos);
 
   const pageMovieUp = async () => {
     state.page = page + 1
@@ -25,7 +30,7 @@ export const useMoviesStore = defineStore('movies', () => {
         params: {
           with_origin_country: 'BR',
           language: 'pt-BR',
-          page: page,
+          page: state.page,
         },
       })
       state.movies = response.data.results
@@ -34,26 +39,63 @@ export const useMoviesStore = defineStore('movies', () => {
     }
   }
 
-  const moviesTop5 = async () => {
+  const moviesTopFiveList = async () => {
     try {
       const response = await api.get('discover/movie', {
         params: {
-          with_origin_country: 'BR',
           language: 'pt-BR',
-          with_original_language: 'pt',
-          page: state.page,
           sort_by: 'vote_average.desc',
           'vote_count.gte': 500,
+          with_origin_country: 'BR',
+          with_original_language: 'pt',
         },
-      })
+      });
 
-      
-      state.movies = response.data.results;
-      const topCinco = state.movies.slice(0,5);
+      state.topFive = (response.data.results).slice(0,5);
     } catch (error) {
-      console.error('Erro filme ', error)
-    }
-  }
+      console.error('Erro top 5 filmes ', error)
+    };
+  };
 
-  return { movies, moviesList, pageMovieUp, pageMovieDown, moviesTop5 }
-})
+  const popularMoviesList = async () => {
+    try {
+      const response = await api.get('discover/movie', {
+        params: {
+          page: state.page,
+          language: 'pt-BR',
+          sort_by: 'popularity.desc',
+          'vote_count.gte': 50,
+          with_origin_country: 'BR',
+          with_original_language: 'pt',
+        },
+      });
+
+      state.popularMovies = response.data.results;
+
+    } catch (error) {
+      console.error('Erro filmes populares', error);
+    };
+  };
+
+  const ultimosLancamentosList = async () => {
+    try {
+      const response = await api.get('discover/movie', {
+        params: {
+          page: state.page,
+          language: 'pt-BR',
+          primary_release_year: 2025,
+          sort_by: 'primary_release_date.desc',
+          'vote_count.gte': 2,
+          with_origin_country: 'BR',
+          with_original_language: 'pt',
+        },
+      });
+
+      state.ultimosLancamentos = response.data.results;
+    } catch (error) {
+      console.error('Erro ultimos lançamentos ', error)
+    };
+  };
+
+  return { movies, topFive, popularMovies, ultimosLancamentos, moviesList, pageMovieUp, pageMovieDown, moviesTopFiveList, popularMoviesList, ultimosLancamentosList };
+});
