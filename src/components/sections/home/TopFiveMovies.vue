babf72a74090710267afac6bd8ee2bfbb27b957f
carloshenriqueok <ifcarloshenrique@gmail.com>
2025-11-30 19:37:12 -0300

    FEAT: section de top 5 filmes terminada [REFS: #8]

--- src/components/sections/home/TopFiveMovies.vue
+++ src/components/sections/home/TopFiveMovies.vue
@@ -1,9 +1,12 @@
 <script setup>
 import { onMounted, computed, ref } from 'vue'
 import { useMoviesStore } from '@/stores/movie'
+import { useGenreStore } from '@/stores/genre'
 
 const moviesStore = useMoviesStore();
-const movieDetails = computed(() => moviesStore.currentMovie)
+const genreStore = useGenreStore();
+
+const movieDetails = computed(() => moviesStore.currentMovie);
 
 // Pega os 5 primeiros filmes melhores avaliados
 const topFive = computed(() => {
@@ -20,43 +23,78 @@ const BackgroundImage = computed(() => {
   return {
     backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.value.backdrop_path})`,
   }
-});
+})
 
 // Função para pegar o indicie (usar nos detalhes do filme)
-const setMovieDetails = async(index) => {
-  currentIndex.value = index;
-  const selectedMovie = topFive.value[index];
+const setMovieDetails = async (index) => {
+  currentIndex.value = index
+  const selectedMovie = topFive.value[index]
 
   await moviesStore.movieDetail(selectedMovie?.id)
 }
 
 onMounted(async () => {
   await moviesStore.moviesTopFiveList();
-  await moviesStore.addLogoToMovie(moviesStore.topFive)
+  await moviesStore.addLogoToMovie(moviesStore.topFive);
+  await genreStore.getAllGenres('movie');
 
-  if(moviesStore.topFive.length > 0) {
+  if (moviesStore.topFive.length > 0) {
     await setMovieDetails(currentIndex.value)
-  }
-});
-
-
+  };
+})
 </script>
 
 <template>
   <section class="top-five" :style="BackgroundImage">
     <div class="title">
       <h2>Top 5 filmes</h2>
-      <p>Confira os 5 filmes mais bem avaliados pelo público</p>
+      <h3>Confira os 5 filmes mais bem avaliados pelo público</h3>
     </div>
     <div class="content">
-      <img :src="`https://image.tmdb.org/t/p/w500${movie?.poster_path}`" :alt="movie?.title">
+      <img
+        class="poster-movie"
+        :src="`https://image.tmdb.org/t/p/w500${movie?.poster_path}`"
+        :alt="movie?.title"
+      />
 
       <div class="infos-movie">
-        <img :src="`https://image.tmdb.org/t/p/original${movie?.logo_path}`" :alt="movie?.title">
+        <img :src="`https://image.tmdb.org/t/p/original${movie?.logo_path}`" :alt="movie?.title" />
         <p>{{ movieDetails?.tagline }}</p>
         <button class="btn-primary">Ver Detalhes</button>
       </div>
 
+      <ul class="ranking-list">
+        <li
+          v-for="(m, index) in topFive"
+          :key="m.id"
+          class="ranking-item"
+          :class="{ active: index === currentIndex }"
+          @click="setMovieDetails(index)"
+          >
+          <div class="ranking-info">
+            <span class="ranking-position">
+              #{{ index + 1 }}
+            </span>
+            <h4>{{ m.title }}</h4>
+            <p class="movie-average" v-if="index === currentIndex">
+              <span class="mdi mdi-star"></span>
+              {{ m.vote_average.toFixed(1) }}
+            </p>
+            <span v-if="index === currentIndex" class="mdi mdi-heart-outline"></span>
+          </div>
+
+          <div class="expanded-details" v-if="index === currentIndex">
+            <p>{{ m.release_date.slice(0, 4) }}</p>
+            <p
+              v-for="g in m.genre_ids.slice(0, 2)"
+              :key="g"
+              >
+              {{ genreStore.getGenreName(g) || '' }}
+            </p>
+          </div>
+
+        </li>
+      </ul>
     </div>
   </section>
 </template>
@@ -88,14 +126,15 @@ onMounted(async () => {
 
 .title {
   & h2 {
-    color: #FFF5EE;
+    color: #fff5ee;
     font-size: 2.5rem;
     font-weight: 600;
   }
-  & p {
-    color: #B2B2B2;
+  & h3 {
+    color: #b2b2b2;
     font-size: 1.1rem;
     margin: 0.7vw 0;
+    font-weight: 500;
   }
 }
 
@@ -104,11 +143,12 @@ onMounted(async () => {
   align-items: center;
   gap: 2vw;
 
-  & img {
+  & .poster-movie {
     height: 20rem;
     width: auto;
     margin-top: 2vw;
     border-radius: 5%;
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
   }
 }
 
@@ -124,10 +164,77 @@ onMounted(async () => {
   & p {
     font-size: 1.1rem;
     max-width: 30vw;
-    color: #FFF5EE;
+    color: #fff5ee;
     margin: 2vw 0;
   }
+}
 
+.ranking-list {
+  display: flex;
+  flex-direction: column;
+  gap: 1vw;
+
+  & .ranking-item {
+    background: linear-gradient(to right, rgba(48, 48, 48, 1), rgba(19, 19, 19, 1));
+    margin-left: 1vw;
+    border-radius: 15px;
+    cursor: pointer;
+    padding: 1vw 7vw 1vw 2vw;
+
+    & .ranking-position {
+      font-size: 1.2rem;
+      color: #fff5ee;
+      font-weight: bold;
+    }
+
+    & .mdi-heart-outline {
+      color: #fff;
+      font-size: 1.3rem;
+      margin-left: 2vw;
+    }
+  }
+}
+
+.ranking-info {
+  display: flex;
+  gap: 1vw;
+  align-items: center;
+
+  & h4 {
+    font-size: 1.2rem;
+    color: #fff5ee;
+    font-weight: 500;
+    width: 60%;
+  }
+
+}
+
+.movie-average {
+    display: flex;
+    align-items: center;
+    background-color: #AFFFD1;
+    color: #767676;
+    font-weight: 500;
+    border-radius: 15px;
+    font-size: 0.9rem;
+    width: 5vw;
+
+    & .mdi-star {
+      font-size: 0.8rem;
+      border-radius: 100%;
+      padding: 0.1vw 0.3vw;
+      background-color: #2DF884;
+      color: #009440;
+      margin-right: 0.3vw;
+    }
+  }
+
+.expanded-details {
+  display: flex;
+  gap: 1vw;
+  color: #b2b2b2;
+  margin: 0.3vw 0 0 2.5vw;
+  font-size: 0.8rem;
 }
 
 </style>
